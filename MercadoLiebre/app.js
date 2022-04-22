39ad4d4dbc332a3e4a8d4e7c8c9f95f4c5f0289a
MPasqui95 <matipasquini95@gmail.com>
2022-04-21 23:47:39 -0300

    MercadoLiebre Entrega Final

--- MercadoLiebre/app.js
+++ MercadoLiebre/app.js
@@ -5,8 +5,8 @@ const path = require ("path");
 const publicPath = path.join (__dirname, "./public");
 app.use (express.static(publicPath));
 
-app.listen (3000, () => {
-    console.log("Servidor 3000 Corriendo");
+app.listen (process.env.PORT || 3000, () => {
+    console.log("Servidor Corriendo");
 });
 
 app.get ("/", (req, res) => {
